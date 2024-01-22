79e55c62cc929bc32583a27f6526ab03183c89a1
tarakaniygod <kolivonka@gmail.com>
2024-01-22 22:54:26 +0300

    feat: completed  task #1

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+// 01-read-file
+
+
+
+const filePath = path.join(__dirname, "text.txt");
+const stream = fs.createReadStream(filePath, "utf-8");
+let data = "";
+stream.on("data", (chunk) => (data += chunk));
+stream.on("end", () => console.log(data));
+stream.on("error", (error) => console.log("Error", error.message));
